cd8c17ea7e9470ffa3bb59709197b17171d8f823
PsChina <panshanzxcv.love@163.com>
2019-04-08 19:06:38 +0800

    update modal

---
+++ components/Modal/demo.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import Modal from './Modal/Modal.jsx'
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      title: 'React Modal',
+      content: '欢迎使用！',
+      visible: false
+    }
+  }
+  openModal(){
+    this.setState({
+      visible: true
+    })
+  }
+  onOk(){
+    this.setState({
+      visible:false
+    })
+  }
+  onCancel(){
+    this.setState({
+      visible:false
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div onClick={this.openModal.bind(this)}>开启弹窗</div>
+        <Modal 
+        width={'600px'}
+        height={'600px'}
+        visible={this.state.visible}
+        title={this.state.title} 
+        content={this.state.content} 
+        onOk={this.onOk.bind(this)} 
+        onCancel={this.onCancel.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+export default App;
